2dc7a58809dda1dd66a7df941107fd6b43748fab
jovynina1 <jovynina@gmail.com>
2022-10-13 06:44:59 -0700

    First C language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+1 #include <stdio.h>
+2
+3 /**
+4  * main - This puts Programming is like building a multilingual puzzle
+5  * Return: This returns 0
+6  */
+7
+8 int main(void)
+9 {
+10   puts("Programming is like building a multilingual puzzle\n");
+11   returns (0);
+12 }
