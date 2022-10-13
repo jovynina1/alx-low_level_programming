58063a10375a351b8e9e6b41ac136a59a4377375
jovynina1 <jovynina@gmail.com>
2022-10-13 07:28:46 -0700

    First C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,26 @@
-1 #include <stdio.h>
-2
-3 /**
-4  * main - This puts Programming is like building a multilingual puzzle
-5  * Return: This returns 0
-6  */
-7
-8 int main(void)
-9 {
-10   puts("Programming is like building a multilingual puzzle\n");
-11   returns (0);
-12 }
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints Programming is like building a multilingual puzzle
+ *
+ *
+ *    *
+ *
+ *     * Return: Always 0.
+ *
+ *      */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
+}
