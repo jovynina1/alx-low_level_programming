7ac624348dd18bc03e83201ea8278e1ee451f950
jovynina1 <jovynina@gmail.com>
2022-10-18 08:45:11 -0700

    3lowercase

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,28 @@
-#include <stdio.h>
+#include "main.h"
+
+
 
 /**
-* main - Prints _putchar
-*
-* Return: Always 0
-*/
+ *
+ *  * main - print "_putchar"
+ *
+ *   *
+ *
+ *    * Return: 0
+ *
+ *     */
 
 int main(void)
 {
-	char s[] = "_putchar\n";
-
-	return (0);
+	_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
 }
+
