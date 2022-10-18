471c6bf164373ee6ead9bc32489d51ef3d22c6d0
jovynina1 <jovynina@gmail.com>
2022-10-18 04:56:40 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,5 +9,6 @@
 int main(void)
 {
 	char s[] = "_putchar\n";
+
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include  "main.h"
+
+/**
+* main - prints alphabets in lowercase
+*
+* Return: return void
+*/
+
+void print_alphabet(void);
+{
+	char ch;
+
+	for (ch = a; ch <= z; ch++)
+		_putchar(ch);
+	putchar('\n')
+}
+		
+		
