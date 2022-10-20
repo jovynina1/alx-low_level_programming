772ab29df6576ceae8abc34e9a6d9f06c092e2db
jovynina1 <jovynina@gmail.com>
2022-10-20 09:07:30 -0700

    Printmost numbers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,20 @@
 
 /**
  * _isdigit - checks for characters that are digits
- * @c: character to check
+ * @c: int to be checked
  * Return: 0 or 1
  */
 
 int _isdigit(int c)
 
 {
+	int y = 0;
+	{
+
 	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+		y = 1;
+	}
+
+	return (y);
 }
+
