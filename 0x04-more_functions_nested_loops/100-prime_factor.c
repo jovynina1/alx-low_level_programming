d28cee500d235c9a3496cfa14921f887d28e8df4
jovynina1 <jovynina@gmail.com>
2022-10-20 14:31:24 -0700

    101-Print Number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+ #include <stdio.h>
+
+/**
+ * prime_factor - Find and print largest prim factor of 612852475143
+ * main - Find and print largest prime factor of 612852475143
+ * Return: 0
+*/
+
+int main(void)
+
+{
+	long int f, n, lf;
+
+	n = 612852475143;
+	f = 3;
+	lf = 0;
+	while (n != 1)
+	{
+	
+		if (n % f == 0)
+		{
+			n = n / f;
+			if (f > lf)
+					if = f;
+
+		}
+		else
+			f++;
+	}
+	printf("%ld\n", lf);
+	return (0);
+}
