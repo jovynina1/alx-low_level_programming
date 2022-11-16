9ce2099ca6462829bb201e9013d407c4ad4fe5ec
jovynina1 <jovynina@gmail.com>
2022-11-16 09:16:21 +0000

    Prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_name - Print the given name
+ * @name: Name to print
+ * @f: Pointer to void function, taking a char * as arg
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
