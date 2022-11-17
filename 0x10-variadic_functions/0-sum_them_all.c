c77a644c2594f23e8756d5f6f8f894ba4c0bfdc4
jovynina1 <jovynina@gmail.com>
2022-11-17 11:15:19 +0000

    Sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,22 +9,20 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	if (n == 0)
-		return (0);
+
+	unsigned int i, sum;
+
+	i = sum = 0;
+		
 
 	va_start(args, n);
 
-	int i, sum = 0;
-
-
-	for (i = 0, i < n; i++;)
+	while (i < n)
 	{
 		sum += va_arg(args, int);
+		i++;
 	}
 	va_end(args);
 	return (sum);
 }
-
-
-
 
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions"
+
+/**
+ * print_strings - print strings with a new line
+ * @n: number of strings passed to the function
+ * @s: string to be printed between the strings
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strngs;
+	char *strgs;
+	unsigned int i;
+
+	va_start(strngs, n);
+	i = 0;
+	
+	while (i < n)
+	{
+		strgs = va_arg(strngs, char *);
